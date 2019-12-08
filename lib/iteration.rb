536a86d96c1950004939dee8f69660a260a56bcb
Austin Scott <austinscott4@gmail.com>
2019-12-08 21:31:12 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -36,17 +36,13 @@ end
 def total_even_pairs(src)
   total = 0
   row_index = 0
-  while row_index < src.count do
-    element_index = 0
-    while element_index < src[row_index].count do
+    while row_index < src.count do
       if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0
         total += src[row_index][0] + src[row_index][1]
       end
-      element_index += 1
+      row_index += 1
     end
-    row_index += 1
-  end
-  total/2
+  total
 end
 
 
